174c66650c10d49b622715e2e4169e5266d00458
Serhii <sergii.ponomarov@gmail.com>
2018-03-04 00:25:17 +0200

    User logs in using google oauth2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+  def self.from_omniauth(auth)
+    email = auth['info']['email']
+    user = User.where(email: email).first
+
+    unless user
+      user = User.create(email: email, password: Devise.friendly_token[0,20])
+    end
+
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "home#index"
   resources :tickets
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
