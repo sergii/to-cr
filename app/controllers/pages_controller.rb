561cd6f50afdf470c59fd2928ab0e2aa706569d7
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2018-06-05 12:44:03 +0300

    Add different gems for user agent parsing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ require 'browser/aliases'
 class PagesController < ApplicationController
   Browser::Base.include(Browser::Aliases)
 
-  def index
+  def sessions
     @sessions = LoginActivity.all
   end
 
@@ -12,5 +12,17 @@ class PagesController < ApplicationController
       request.env['HTTP_USER_AGENT'],
       accept_language: request.env['HTTP_ACCEPT_LANGUAGE']
     )
+  end
+
+  def device_detector
+    @client = DeviceDetector.new(request.env['HTTP_USER_AGENT'])
+  end
+
+  def user_agent_parser
+    @user_agent_parser = UserAgentParser.parse(request.env['HTTP_USER_AGENT'])
+  end
+
+  def useragent
+    @user_agent = UserAgent.parse(request.env['HTTP_USER_AGENT'])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "home#index"
   resources :tickets
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get '/sessions' => 'pages#index'
-  get '/browser' => 'pages#browser'
+  get '/sessions', to: 'pages#sessions'
+  get '/browser', to: 'pages#browser'
+  get '/device_detector', to: 'pages#device_detector'
+  get '/user_agent_parser', to: 'pages#user_agent_parser'
+  get '/useragent', to: 'pages#useragent'
 end
